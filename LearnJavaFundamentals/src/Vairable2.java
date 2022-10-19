f4b7c320d214035446fd4a1ed480f7d6dce8ced5
Shivendu Kumar <shivendusingh2002@gmail.com>
2022-10-19 07:55:10 +0530

    Wrappper Class in java

---
+++ LearnJavaFundamentals/src/Vairable2.java
@@ -0,0 +1,15 @@
+
+public class Vairable2 {
+public static void main(String[] args)
+{
+	// primitive datatypes
+	int i = 1;
+	double d = 1.2;
+	
+	//Their is integers class and other datatype class  c  allled wrapper class
+	
+	Integer i1 =1;// this means Integer i1 - new Integer(1);
+	Double d1 = 1.2;//Double d1 = new Double(1.2);
+	System.out.println(d1.intValue());	
+}
+}
